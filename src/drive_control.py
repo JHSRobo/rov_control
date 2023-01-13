844a9ec0e820dc73753650da48b26444968b1fdc
Alex Bertran <70992408+ApBertran@users.noreply.github.com>
2023-01-12 22:39:49 -0800

    Depth hold integration into thrusters

--- src/drive_control.py
+++ src/drive_control.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 # author Adon Sharp | v0.2 | 1-15-2022
 # author Alex Bertran | v1.0 | 10-29-2022
+# author Alex Bertran | v1.1 | 1-12-2023
 
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
-from std_msgs.msg import UInt8  # For camera  pub
-from std_msgs.msg import Bool
+from std_msgs.msg import Float32 # for depth hold
 from math import copysign
 from copilot_interface.msg import controlData
+from copilot_interface.msg import autoControlData
 from rov_control.msg import thrusterPercents
 
 rospy.init_node("drive_control")
@@ -16,6 +17,8 @@ rospy.init_node("drive_control")
 sensitivity = {"linear": 0.5, "angular": 0.25, "vertical": 0.5} # Holds a percent multiplier for ROV sensitivity
 
 thrustEN = False  # thrusters enabled (True = yes, False = default = no
+dhEnable = False
+depthHoldThrust = 0.0
 
 #The vector that gets edited by the callbacks and then published
 joy_vector = Twist()
@@ -79,44 +82,48 @@ def translate_vectors(vector):
 
 
 def joystick_callback(joy):
-    global camera_select, joy_vector, sensitivity, thrustEN
-    
-    # If thrusters enabled, map the joystick inputs to the joy_vector
-    if thrustEN:
-        # Multiply LR axis by -1 in base position (front-front, etc.)to make right positive
-        # NOTE: right and rotate right are negative on the joystick's LR axis
-        l_axisLR = joy.axes[0] * sensitivity['linear'] * -1
-        l_axisFB = joy.axes[1] * sensitivity['linear']
-        a_axis = joy.axes[2] * sensitivity['angular'] * -1 
+  global joy_vector, sensitivity, thrustEN
 
-        # Apply the exponential ratio on all axis
-        a_axis = expDrive(a_axis)
-        l_axisLR = expDrive(l_axisLR)
-        l_axisFB = expDrive(l_axisFB)
+  # If thrusters enabled, map the joystick inputs to the joy_vector
+  if thrustEN:
+    # Multiply LR axis by -1 in base position (front-front, etc.)to make right positive
+    # NOTE: right and rotate right are negative on the joystick's LR axis
+    l_axisLR = joy.axes[0] * sensitivity['linear'] * -1
+    l_axisFB = joy.axes[1] * sensitivity['linear']
+    a_axis = joy.axes[2] * sensitivity['angular'] * -1 
 
-    else:
-        a_axis = 0
-        l_axisLR = 0
-        l_axisFB = 0
+    # Apply the exponential ratio on all axis
+    a_axis = expDrive(a_axis)
+    l_axisLR = expDrive(l_axisLR)
+    l_axisFB = expDrive(l_axisFB)
 
-    # Add the created vectors to the joy_vector
-    joy_vector.linear.x = l_axisLR
-    joy_vector.linear.y = l_axisFB
-    joy_vector.angular.x = a_axis
+  else:
+    a_axis = 0
+    l_axisLR = 0
+    l_axisFB = 0
 
-    # Translate to thrusterPercents msg and publish
-    thrust_percents = translate_vectors(joy_vector)
-    vel_pub.publish(thrust_percents)
+  # Add the created vectors to the joy_vector
+  joy_vector.linear.x = l_axisLR
+  joy_vector.linear.y = l_axisFB
+  joy_vector.angular.x = a_axis
+
+  # Translate to thrusterPercents msg and publish
+  thrust_percents = translate_vectors(joy_vector)
+  vel_pub.publish(thrust_percents)
 
 # Callback that runs whenever the throttle sends an update
 def throttle_callback(joy):
-  global thrustEN, joy_vector, sensitivity
+  global thrustEN, joy_vector, sensitivity, dhEnable, depthHoldThrust
 
   # check if thrusters disabled
   if thrustEN:
-    v_axis = joy.axes[2] * sensitivity['vertical'] * -1
+    if dhEnable:
+      v_axis = depthHoldThrust / 10
+    
+    else:
+      v_axis = joy.axes[2] * sensitivity['vertical'] * -1
 
-    v_axis = expDrive(v_axis)
+      v_axis = expDrive(v_axis)
   else:
     v_axis = 0
 
@@ -131,21 +138,29 @@ def throttle_callback(joy):
 # Callback to anything published by the dynamic reconfigure copilot page
 def control_callback(control):
   global thrustEN, sensitivity
-  
+
   thrustEN = control.thruster_status
   sensitivity['linear'] = control.linear_sense
   sensitivity['angular'] = control.angular_sense
   sensitivity['vertical'] = control.vertical_sense
 
-  return control
+def auto_control_callback(auto_control):
+  global dhEnable
+  dhEnable = auto_control.dh_status
+
+def depth_hold_callback(pid_effort):
+  global depthHoldThrust
+  depthHoldThrust = pid_effort.data
 
 if __name__  == "__main__":
-    global horizJoySub, vertJoySub, vel_pub, camera_select
-    horiz_joy_sub = rospy.Subscriber('joystick', Joy, joystick_callback)
-    vert_joy_sub = rospy.Subscriber('throttle', Joy, throttle_callback)
-    control_sub = rospy.Subscriber('control', controlData, control_callback)
-    
-    vel_pub = rospy.Publisher('thrusters', thrusterPercents, queue_size=1)
+  global horizJoySub, vertJoySub, vel_pub, control_sub, auto_control_sub, depth_hold_sub
+  horiz_joy_sub = rospy.Subscriber('joystick', Joy, joystick_callback)
+  vert_joy_sub = rospy.Subscriber('throttle', Joy, throttle_callback)
+  control_sub = rospy.Subscriber('control', controlData, control_callback)
+  auto_control_sub = rospy.Subscriber('auto_control', autoControlData, auto_control_callback)
+  depth_hold_sub = rospy.Subscriber('pid_effort', Float32, depth_hold_callback)
+
+  vel_pub = rospy.Publisher('thrusters', thrusterPercents, queue_size=1)
 
     # Enter the event loop
     rospy.spin()
