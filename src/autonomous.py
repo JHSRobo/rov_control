55a2909d56b36395eb10a5f67615b9d00b142872
Alex Bertran <70992408+ApBertran@users.noreply.github.com>
2023-04-15 14:42:53 -0700

    testing renaming structure

--- src/autonomous.py
+++ src/autonomous.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # author Alex Bertran '24 | v0.0 | 11-11-2022 | prototyping autonomous infrastructure
 # author Alex Bertran '24 | v1.0 | 1-12-2023 | functional depth hold fully integrated
+# author Alex Bertran '24 | v1.1 | 4-15-2023 | automonous docking for the 22-23 season
 #import sys
 #import os
 #sys.path.remove(os.path.dirname(__file__))
@@ -9,7 +10,8 @@ import rospy
 from geometry_msgs.msg import Twist # may be necessary in the future, delete after 3/1/2023
 from simple_pid import PID  # For pids
 from std_msgs.msg import Float32  # For depth sensor
-from copilot_interface.msg import autoControlData, controlData# /auto_control custom message
+from copilot_interface.msg import autoControlData, controlData # /auto_control custom message
+from rov_control.msg import autoDock
 
 rospy.init_node("autonomous_control")
 
@@ -17,13 +19,13 @@ rospy.init_node("autonomous_control")
 thrustEN = False  # Thruster toggle status
 dhEnable = False  # Depth hold toggle status
 targetDepth = 7.5  # Target depth for depth hold
-p_scalar = 1.0  # PID: proportional
-i_scalar = 0.1  # PID: integral
-d_scalar = 0.05  # PID: derivative
+dh_p_scalar = 1.0  # PID: proportional
+dh_i_scalar = 0.1  # PID: integral
+dh_d_scalar = 0.05  # PID: derivative
 
 # Create new pid object with scalars and target point
-pid = PID(p_scalar, i_scalar, d_scalar, setpoint=targetDepth)
-pid.output_limits = (-10, 10)
+dh_pid = PID(dh_p_scalar, dh_i_scalar, dh_d_scalar, setpoint=targetDepth)
+dh_pid.output_limits = (-10, 10)
 
 # update variables with data from "/control" topic
 def rovDataCallback(data):
@@ -32,36 +34,37 @@ def rovDataCallback(data):
 
 # update variables with data from "/auto_control" topic
 def autoDataCallback(data):
-  global dhEnable, targetDepth, p_scalar, i_scalar, d_scalar, pid
+  global dhEnable, targetDepth, dh_p_scalar, dh_i_scalar, dh_d_scalar, dh_pid
   dhEnable = data.dh_status
   targetDepth = data.target_depth
-  p_scalar = data.p_scalar
-  i_scalar = data.i_scalar
-  d_scalar = data.d_scalar
+  dh_p_scalar = data.p_scalar
+  dh_i_scalar = data.i_scalar
+  dh_d_scalar = data.d_scalar
   
   # Slightly redundant code but other variables should stay in case of future use
-  pid.setpoint = targetDepth
-  pid.tunings = (p_scalar, i_scalar, d_scalar)
+  dh_pid.setpoint = targetDepth
+  dh_pid.tunings = (dh_p_scalar, dh_i_scalar, dh_d_scalar)
   
 def change_depth_callback(depth):
-  global dhEnable, thrustEN, targetDepth, pid, pid_pub
+  global dhEnable, thrustEN, targetDepth, dh_pid, dh_pid_pub
  
   if thrustEN and dhEnable:
     # calibration of pressure sensor | REMEMBER TO KEEP UP TO DATE
     #currentDepth = abs((depth.data - 198.3) / (893.04 / 149))
     currentDepth = abs((depth.data + 1.94) * 3.281) # Convert to feet and calibrate
-    calculation = pid(currentDepth)
-    pid_pub.publish(calculation)
+    calculation = dh_pid(currentDepth)
+    dh_pid_pub.publish(calculation)
   
 def main():
-  global depth_sub, control_status_sub, auto_control_status_sub, pid_pub
+  global depth_sub, control_status_sub, auto_control_status_sub, auto_dock_sub, dh_pid_pub
   
   depth_sub = rospy.Subscriber('rov/depth_sensor', Float32, change_depth_callback)
   control_status_sub = rospy.Subscriber('control', controlData, rovDataCallback)
   auto_control_status_sub = rospy.Subscriber('auto_control', autoControlData, autoDataCallback)
+  auto_dock_sub = rospy.Subscriber('auto_dock_data', autoDock, autoDockCallback)
   
   # Depth hold PID value publisher
-  pid_pub = rospy.Publisher('pid_effort', Float32, queue_size=1)
+  dh_pid_pub = rospy.Publisher('pid_effort', Float32, queue_size=1)
   
   rospy.spin()
 
